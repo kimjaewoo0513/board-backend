5678a9447486e41a4644e13600d34a991663cb38
kimjaewoo0513 <howchamsin@gmail.com>
2023-04-19 19:48:42 +0900

    코멘트 기능 개발중

--- src/main/java/com/demo/comment/dto/param/CommentListParam.java
+++ src/main/java/com/demo/comment/dto/param/CommentListParam.java
@@ -4,18 +4,18 @@ import com.demo.bbs.dto.param.PageParam;
 
 public class CommentListParam extends PageParam{
 	
-	 private Integer bbsSeq; // 게시글 번호
+	private Integer bbsSeq; // 게시글 번호
 
-	    public CommentListParam(Integer bbsSeq) {
-	        this.bbsSeq = bbsSeq;
-	    }
-
-	    public Integer getBbsSeq() {
-	        return bbsSeq;
-	    }
-
-	    public void setBbsSeq(Integer bbsSeq) {
-	        this.bbsSeq = bbsSeq;
-	    }
+	public CommentListParam(Integer bbsSeq) {
+	    this.bbsSeq = bbsSeq;
+	}
+	
+	public Integer getBbsSeq() {
+	    return bbsSeq;
+	}
+	
+	public void setBbsSeq(Integer bbsSeq) {
+	    this.bbsSeq = bbsSeq;
+	}
 
 }
--- src/main/java/com/demo/comment/service/CommentService.java
+++ src/main/java/com/demo/comment/service/CommentService.java
@@ -1,15 +1,23 @@
 package com.demo.comment.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.demo.comment.dao.CommentDao;
 import com.demo.comment.dto.request.CommentRequest;
 import com.demo.comment.dto.response.CommentResponse;
 
 @Service
 public class CommentService {
+	
+	@Autowired
+	private CommentDao dao;
 
+	/* 댓글 조회 */
 	public CommentResponse getBbsCommentList(CommentRequest req) {
-		// TODO Auto-generated method stub
+		
+		
+		// TODO dao 호출 코드 짜기
 		return null;
 	}
 
